47e9a80dfb772514e9cf5ee1238b0dd0bcb2520c
marlon20m <marlonmora.ndr@gmail.com>
2020-06-30 19:31:47 -0500

    adding Obi Wan Kenobi

--- server1.js
+++ server1.js
@@ -21,6 +21,12 @@ var darthmaul = {
   forcePoints: 1200
 };
 
+var obiwankenobi = {
+  name: "Obi Wan Kenobi",
+  role: "Jedi Master",
+  age: 390,
+  forcePoints: 1000
+};
 // Create one more data entry for the character Obi Wan Kenobi.
 // Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
 //
@@ -31,18 +37,21 @@ var darthmaul = {
 
 // Routes
 // ===========================================================
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-app.get("/yoda", function(req, res) {
+app.get("/yoda", function (req, res) {
   res.json(yoda);
 });
 
-app.get("/darthmaul", function(req, res) {
+app.get("/darthmaul", function (req, res) {
   res.json(darthmaul);
 });
 
+app.get("/obiwankenobi", function (req, res) {
+  res.json(obiwankenobi);
+});
 // Create a new Express route that leads users to the new Obi Wan Kenobi Data
 // Follow the same format as the Yoda and Darth Maul routes
 //
@@ -53,6 +62,6 @@ app.get("/darthmaul", function(req, res) {
 
 // Listener
 // ===========================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
